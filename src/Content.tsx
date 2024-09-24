5eda01f1f4cd414cb3a3c8355d7f3add4c5b2034
Tom Lawton <tom.lawton@aprilsix.com>
2024-09-24 10:42:05 +0100

    More structural updates to place logic in better locations

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,18 +1,9 @@
 import { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
 
 import { LocationList } from "./screens/LocationList";
 import { Weather } from "./screens/Weather";
 
 import { location } from "./types/location";
-
-const fetchLocations = async (userLocation: string) => {
-    const response = await fetch(
-        `https://geocoding-api.open-meteo.com/v1/search?name=${userLocation}&count=10&language=en&format=json`
-    );
-
-    return await response.json();
-};
 
 interface ContentProps {
     userLocation: string;
@@ -24,13 +15,6 @@ export const Content: React.FC<ContentProps> = ({ userLocation }) => {
         setLocation(null);
     }, [userLocation]);
 
-    // Queries
-    const { data, error, isLoading } = useQuery({
-        queryKey: ["locations", userLocation],
-        queryFn: () => {
-            return fetchLocations(userLocation);
-        },
-    });
     //If the user has yet to submit a location, display a message
     if (!userLocation) {
         return (
@@ -40,24 +24,13 @@ export const Content: React.FC<ContentProps> = ({ userLocation }) => {
         );
     }
 
-    //If there's been an error, display it
-    if (error) {
-        return <p className="error">{error.message}</p>;
-    }
-
     //If there's a location selected, display the weather
     if (location) {
         return <Weather location={location} />;
     }
 
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
-
-    if (!data?.results || data.results.length === 0) {
-        return <p>No locations found, please try a different search</p>;
-    }
-
     //If there's data, display the location list
-    return <LocationList locations={data.results} setLocation={setLocation} />;
+    return (
+        <LocationList userLocation={userLocation} setLocation={setLocation} />
+    );
 };
